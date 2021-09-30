1f5baea4efea58f0896e2a8696d79047e0fddfef
David Josué Marcial Quero <A00828702@itesm.mx>
2021-09-30 16:34:11 -0700

    Initial Commit

---
+++ App04-Listas/App04_ListasApp.swift
@@ -0,0 +1,17 @@
+//
+//  App04_ListasApp.swift
+//  App04-Listas
+//
+//  Created by David Josué Marcial Quero on 30/09/21.
+//
+
+import SwiftUI
+
+@main
+struct App04_ListasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
